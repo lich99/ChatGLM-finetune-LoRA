2a02c183d1e9ea86f30a9a2398978d1ff098b6cc
Chenghai LI <lichenghai99@gmail.com>
2023-03-29 10:47:54 +0800

    update loss calculation

--- lora_utils/insert_lora.py
+++ lora_utils/insert_lora.py
@@ -1,7 +1,7 @@
 import numpy as np
 import loralib as lora
 
-def get_lora_model(model, lora_config):
+def get_lora_model_loralib(model, lora_config):
     
     # lora_config = {
     #     'r': 32,
@@ -24,6 +24,77 @@ def get_lora_model(model, lora_config):
                 qkv_proj.load_state_dict(module.query_key_value.state_dict(), strict=False)
                 module.query_key_value = qkv_proj
 
+
+    lora.mark_only_lora_as_trainable(model)
+
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    trainable_params = sum([np.prod(p.size()) for p in model_parameters])
+
+    model_parameters = filter(lambda p: not p.requires_grad, model.parameters())
+    non_trainable_params = sum([np.prod(p.size()) for p in model_parameters])
+
+    print('trainable_params:{} ({:.2f}%), non_trainable_params:{}'.format(trainable_params, trainable_params/non_trainable_params*100,non_trainable_params))
+
+    return model
+
+
+
+import peft
+import torch
+from peft import LoraConfig
+from itertools import compress
+
+
+class QKV_layer(torch.nn.Module):
+    def __init__(self, in_features, out_features):
+        super(QKV_layer, self).__init__()
+        self.linear_q = torch.nn.Linear(in_features, out_features//3)
+        self.linear_k = torch.nn.Linear(in_features, out_features//3)
+        self.linear_v = torch.nn.Linear(in_features, out_features//3)
+
+    def update(self, target_layer):
+        self.linear_q.weight.data = target_layer.weight[:target_layer.out_features//3, :].data
+        self.linear_q.bias.data = target_layer.bias[:target_layer.out_features//3].data
+
+        self.linear_k.weight.data = target_layer.weight[target_layer.out_features//3:target_layer.out_features//3*2, :].data
+        self.linear_k.bias.data = target_layer.bias[target_layer.out_features//3:target_layer.out_features//3*2].data
+
+        self.linear_v.weight.data = target_layer.weight[target_layer.out_features//3*2:, :].data
+        self.linear_v.bias.data = target_layer.bias[target_layer.out_features//3*2:].data
+    
+    def forward(self, x):
+        q = self.linear_q(x)
+        k = self.linear_k(x)
+        v = self.linear_v(x)
+        return torch.concat([q,k,v], dim = -1)
+
+
+
+def get_lora_model(model, lora_config):
+    
+    target_modules = list(compress(['q', 'k', 'v'], lora_config['enable_lora']))
+
+    config = LoraConfig(
+              peft_type="LORA", 
+              task_type="SEQ_2_SEQ_LM", 
+              r=lora_config['r'], 
+              lora_alpha=lora_config['lora_alpha'], 
+              target_modules=["q", "v"],
+              lora_dropout=lora_config['lora_dropout'])
+
+
+    for key, module in model.named_modules():
+        if key.endswith('attention'):
+            layer = int(key.split('.')[2])
+            try:
+                qkv_layer = QKV_layer(module.query_key_value.in_features, module.query_key_value.out_features)
+                qkv_layer.update(module.query_key_value)
+                module.query_key_value = qkv_layer
+                module.query_key_value = peft.tuners.lora.LoraModel(config, module.query_key_value)
+            except:
+                print('[Warn] Passed')
+                pass
+        
 
     lora.mark_only_lora_as_trainable(model)
 
--- train_new.py
+++ train_new.py
@@ -30,8 +30,8 @@ warm_up_ratio = 0.1
 
 
 
-tokenizer = AutoTokenizer.from_pretrained(checkpoint, trust_remote_code=True)
-model = AutoModel.from_pretrained(checkpoint, trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint, trust_remote_code=True, revision = 'main')
+model = AutoModel.from_pretrained(checkpoint, trust_remote_code=True, revision = 'main')
 model = lora_utils.insert_lora.get_lora_model(model, lora_config)
 
 deepspeed_plugin = DeepSpeedPlugin(zero_stage=2, gradient_accumulation_steps=accumulate_step)
@@ -43,8 +43,8 @@ GLM_Data.device = device
 import dataset.Alpaca as Alpaca_Data
 
 pairs = Alpaca_Data.load('./data/alpaca_data.json')
-pairs = list(filter(lambda pair: len(pair['prompt'])+len(pair['completion']) <= max_length, pairs))
 pairs_encoded = GLM_Data.encode_pairs(pairs, tokenizer)
+pairs_encoded = list(filter(lambda pair: len(pair['prompt'])+len(pair['completion']) <= max_length, pairs_encoded))
 train_dataset = GLM_Data.GLMDataset(pairs_encoded)
 train_dataloader = DataLoader(dataset=train_dataset, collate_fn = GLM_Data.collate_fn, shuffle=True, batch_size=batch)
 
@@ -62,13 +62,16 @@ model, optimizer, train_dataloader = accelerator.prepare(model, optimizer, train
 model.to(device).train()
 
 
+total_step = 0
+effective_step = 0
+
 for epoch in range(NUM_EPOCHS):
-    total_loss = 0
+    epoch_loss_local = 0
     for step, batch in enumerate(t:=tqdm.tqdm(train_dataloader)):
         outputs = model(**batch)
         loss_d = outputs.loss.detach()
-        t.set_description(f"loss: {loss_d.cpu().float().numpy()}")
         epoch_loss_local += loss_d
+        t.set_description(f"loss: {epoch_loss_local.cpu().float() / step}")
         loss = outputs.loss / accumulate_step
         accelerator.backward(loss)
         if (step+1) % accumulate_step == 0:
@@ -77,7 +80,16 @@ for epoch in range(NUM_EPOCHS):
             optimizer.zero_grad()
 
     accelerator.wait_for_everyone()
+    all_epoch_loss, all_step = accelerator.gather((epoch_loss_local, torch.tensor(step, device=device)))
+
     if accelerator.is_main_process:
-        peft_model_id = f"finetune_{epoch}"
-        accelerator.save(lora.lora_state_dict(accelerator.unwrap_model(model)), '/saved/'+peft_model_id+'.pt')
-    
+        model_id = f"finetune_{epoch}"
+        accelerator.save(lora.lora_state_dict(accelerator.unwrap_model(model)), '/saved/'+model_id+'.pt')
+        
+        epoch_loss = all_epoch_loss.float().sum() / (all_step+1).sum()
+        total_step += (all_step+1).sum()
+        effective_step += ((all_step+1)//accumulate_step).sum()
+        print(f'epoch: {epoch}, step {effective_step.cpu().numpy()}, training_loss: {epoch_loss.cpu().numpy()}')
+        
+    accelerator.wait_for_everyone()
+
